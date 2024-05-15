1cf61f9b993f91dfdc7b31be0afdab97e8654784
karivas-dev <karenrivasg17@gmail.com>
2024-05-14 22:04:10 -0600

    small fix on error directive

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,9 @@
-@props(['titulo', 'name'])
+@props(['titulo'])
 
 <div class="inline-block align-top my-4">
     <label class="block">{{ $titulo }}</label>
-    <input {{ $attributes }} name="{{$name}}" class="font-common text-black bg-white border hover border-slate-300 shadow-inner hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-5 py-1 shadow-xl">
-    @error(' {{ $name }}' )
+    <input {{ $attributes }} class="font-common text-black bg-white border hover border-slate-300 shadow-inner hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-5 py-1 shadow-xl">
+    @error($attributes['name'])
         <p>{{ $message }} </p>
     @enderror
 </div>
