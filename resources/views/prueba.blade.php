676fdd4d41f6ad554aba818c4114aed6985767f9
Alisson-Diaz <153238496+Alisson-Diaz@users.noreply.github.com>
2024-05-28 18:15:07 -0600

    commit product card/prueba

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,2 +1,44 @@
+<x-layout>
 
+
+<nav class="shadow-xl mt-10">
+    <span class="material-symbols-outlined  w-40 ">
+        menu
+        </span>
+
+        <form class="max-w-md mx-auto">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="  Buscar" required />
+
+            </div>
+        </form>
+
+</nav>
+
+ <div>
+
+     <a  class=" block max-w-sm p-6  shadow  dark:border-black border-2 rounded mt-20 ">
+
+        <img src="https://i.pinimg.com/originals/76/bc/18/76bc18531ec18c6ab04e6b24f8f108ec.jpg" class="rounded ">
+
+        <hnp5 class="font-title mb-2 text-2xl font-bold tracking-tight dark:text-black ">Tecomata</h5>
+         <p class="dark:text-black font-common rounded mt-7">La tecomata es un tecomate decorado como gallina</p>
+
+  <h3 class="font-common text-base mt-10">Precio</h3>
+
+    </a>
+
+</div>
+
+
+
+
+
+</x-layout>
 
