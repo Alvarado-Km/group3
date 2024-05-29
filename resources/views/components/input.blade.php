0a236b0ad3934387982d899c8d058ce54da0dddd
Alisson-Diaz <153238496+Alisson-Diaz@users.noreply.github.com>
2024-05-28 19:01:09 -0600

    commit resources/vew

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,9 +1,14 @@
 @props(['titulo'])
 
+@layer @component('')
+
+@endcomponent
 <div class="inline-block align-top my-4">
     <label class="block">{{ $titulo }}</label>
-    <input {{ $attributes }} class="font-common text-black bg-white border hover border-slate-300 shadow-inner hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-5 py-1 shadow-xl">
+    <input {{ $attributes }} class="font-common text-black bg-white border hover border-slate-300  hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-5 py-1 shadow-xl">
     @error($attributes['name'])
         <p>{{ $message }} </p>
     @enderror
+
+
 </div>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
- 
-
+    
+<h1>!Bienvenido a nuestro sitio web¡</h1>
+<h1>¿Qué deseas hacer?</h1>
 </x-layout>
