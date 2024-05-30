bfd396b2e5e5b92295cdc0265f19915f5c95c61a
Alisson-Diaz <153238496+Alisson-Diaz@users.noreply.github.com>
2024-05-30 15:46:49 -0600

    commit Prueba/product card

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -2,15 +2,13 @@
 
 
 <nav class="shadow-xl mt-10">
-    <span class="material-symbols-outlined  w-40 ">
-        menu
-        </span>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" class="fill-black "/></svg>
 
-        <form class="max-w-md mx-auto">
+        <form class="max-w-md mx-auto ">
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
                 </div>
@@ -23,7 +21,7 @@
 
  <div>
 
-     <a  class=" block max-w-sm p-6  shadow  dark:border-black border-2 rounded mt-20 ">
+     <a  class=" block max-w-sm p-6  shadow  border-black border rounded mt-20  ">
 
         <img src="https://i.pinimg.com/originals/76/bc/18/76bc18531ec18c6ab04e6b24f8f108ec.jpg" class="rounded ">
 
