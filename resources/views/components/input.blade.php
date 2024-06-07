5acc614f3efb7553c431a0cc59c14bdbce4da14a
xavier-salmeron <153401889+xavier-salmeron@users.noreply.github.com>
2024-06-07 14:00:49 -0600

    cambio frontend sigunp

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -4,7 +4,7 @@
     <label class="block">{{ $titulo }}</label>
     <input {{ $attributes }} class="disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
     invalid:border-pink-500 invalid:text-pink-600
-    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 font-common text-black bg-white border hover border-slate-300  hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-2 py-1 shadow-xl">
+    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 font-common text-black bg-white border hover border-slate-300  hover:bg-[#F5DEB3] active:bg-[#966919] focus:outline-none focus:ring focus:ring-[#988558] rounded-lg px-2 -py-1 shadow-xl">
     @error($attributes['name'])
         <p>{{ $message }} </p>
     @enderror
