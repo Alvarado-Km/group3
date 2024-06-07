2b5514b8b7da89c14e73c3e031b4e415c57a299c
xavier-salmeron <153401889+xavier-salmeron@users.noreply.github.com>
2024-06-07 14:22:01 -0600

    sigunp terminado

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,18 +1,5 @@
 <x-layout>
 
 
-
-<!-- @foreach ($productos as $product)
-    <h1>{{$product->name}}</h1>
-    <p>Categoría</p>
-    <p>
-        {{ $product->category->name }}
-    </p>
-        <form action="/product/{{ $product->id }}" method="post">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Borrar</button>
-        </form>
-@endforeach -->
 </x-layout>
 
