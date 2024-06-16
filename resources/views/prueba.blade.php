368bbd17e3dc1afe01072d8d7d30b39ae20e42c0
Alisson-Diaz <153238496+Alisson-Diaz@users.noreply.github.com>
2024-06-16 11:51:11 -0600

    products/product commit

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,51 +1,37 @@
 <x-layout>
-    <div>
 
-        <nav class=" ml-3 ">
-
-            <div>
-                <h1 class="flex justify-center  text font-title text-5xl ">Velas</h1>
-            </div>
-
-
-            <div  >
-                <svg class="absolute bottom-94" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                    fill="#5f6368">
-                    <path
-                        d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
-                </svg>
-
-            </div>
-
-
-            <svg class="size-10 cursor-pointer fill-black" xmlns="http://www.w3.org/2000/svg" height="24px"
-                viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
-            </svg>
-            <form class="ml-10">
-                <label for="default-search"
-                    class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 " aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="search" id="default-search"
-                        class="  block w-5/6 p-4 ps-10 text-sm text-gray-500 border border-gray-300 rounded-lg   dark:bg-gray-300  dark:text-white  "
-                        placeholder="  Buscar" required />
-
-                </div>
-            </form>
-
-        </nav>
+<nav class="Mt-3 flex justify-around">
+    <svg class="fill-black size-10" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/></svg>
+    <h1 class="Font-title text-4xl ">ArtisanRoots</h1>
+    <svg class="fill-black size-10 " xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/></svg>
+    <svg class="fill-black size-10" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>
+    <svg class="fill-black size-10" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z"/></svg>
     </div>
 
+</nav>
+
+<div class="grid grid-cols-5 gap-5  ">
+
+    <div class="ml-10 mt-10">
+
+          <img class="  cursor-pointer  w-25 h-25 rounded-full" src="/img/Velas.png" alt="Rounded avatar">
+          <h1 class="font-common text-2xl border boder-gray bg-gray-300 rounded text-center mt-3"> José Herrera</h1>
+          <p class="font-common"> Mi nombre es José Herrero desde que era niño, la artesanía ha sido parte integral de mi vida gracias a mi abuelo, un experto tejedor de mimbre. Pasaba horas observándolo trabajar en su taller, maravillado por su habilidad y la belleza de sus creaciones. Con el tiempo, comencé a experimentar por mi cuenta, practicando el tejido de mimbre y explorando mi propia creatividad. Cada trenzado era una oportunidad para conectarme con la naturaleza que me rodeaba, y pronto descubrí que la artesanía no era solo un pasatiempo, sino una forma de expresión y conexión con mi entorno.  leer más...</p>
+   </div>
+
+  <div >
+
+  </div>
+
+  <div>
+
+  </div>
+
+   <div>
 
 
-    <x-card title="tecomata" description="Es un tecomate decorado como gallina" price="6.99"></x-card>
-    <x-card title="tecomata" description="Es un tecomate decorado como gallina" price="6.99"></x-card>
+  <div>
+
+
 
 </x-layout>
