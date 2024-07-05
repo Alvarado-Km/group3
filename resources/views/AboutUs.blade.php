fbf16744f00d63791f51d9c18c997819936d214c
Alvarado-Km <158789271+Alvarado-Km@users.noreply.github.com>
2024-07-05 14:59:42 -0600

    color palette

--- resources/views/AboutUs.blade.php
+++ resources/views/AboutUs.blade.php
@@ -2,16 +2,34 @@
 
     <x-primarynav> </x-primarynav>
 
-    <div class="grid place-items-center">
-        <img src="img/momentaryBg.jpg">
+    <div class="grid grid-cols-6 grid-rows-1">
+        <div class="grid col-start-1 col-span-3 grid place-items-center ">
+            <img src="img/imagen.jpg">
+        </div>
+
+        <div class="grid place-items-center col-start-4 col-span-6 mt-16">
+            <h2 class="text-4xl font-title mt-5"> ¿Quiénes somos? </h2>
+            <p class="text-xl my-48 mx-48 text-justify font-common mt-8">Somos Artesía, y nos dedicamos a preservar y
+                promover el arte artesanal, colaborando directamente con
+                talentosos artesanos para ofrecer productos únicos y de alta calidad. Valoramos la sostenibilidad y el
+                comercio
+                justo, creando un impacto positivo en las comunidades artesanas. Descubre piezas auténticas y hechas a
+                mano
+                que
+                cuentan historias y reflejan dedicación. </p>
+
+        </div>
     </div>
 
-    <div class="grid place-items-center">
-    <h2 class="text-4xl font-title mt-5" > ¿Quiénes somos? </h2>
-    <p class="text-xl my-48 mx-48 text-justify font-common mt-8">Somos Artesía, y nos dedicamos a preservar y promover el arte artesanal, colaborando directamente con
-        talentosos artesanos para ofrecer productos únicos y de alta calidad. Valoramos la sostenibilidad y el comercio
-        justo, creando un impacto positivo en las comunidades artesanas. Descubre piezas auténticas y hechas a mano que
-        cuentan historias y reflejan dedicación. </p>
+    <div>
+        <img src="img/example.jpeg" class="w-screen h-min transition transition-colors">
+    </div>
+
+    <div>
+<h1 class="mt-24 font-title text-4xl grid place-items-center mb-20">Sobre nosotros </h1>
+    </div>
+    <div>
+        <img src="/img/mision.">
     </div>
 
 </x-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ export default {
     theme: {
       extend: {
         backgroundImage: {},
+        colors:{
+            red: '#b32718',
+            orange: '#f28425',
+            salmon: '#ee4b2a',
+            yellow: '#fdac41',
+            paleGreen: '#b8a944',
+            grass: '#7c6e2d',
+            mildew: '#4c4926'
+        },
+        borderWidth:{
+            '20': '20px',
+            '30': '30px'
+
+        },
         fontFamily: {
             'title': ['Ribeye', ...defaultTheme.fontFamily.sans],
             'common': ['Slabo27px', ...defaultTheme.fontFamily.serif]
@@ -21,6 +35,6 @@ export default {
     plugins: [],
   }
 
- 
+
 
 
