7aea708276a9cddcff1ad3479df58a69ae3f5f17
Alvarado-Km <158789271+Alvarado-Km@users.noreply.github.com>
2024-07-12 14:28:23 -0600

    variable src added

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,8 @@
- @props(['title', 'description','price'])
+ @props(['title', 'description','price', 'src'])
 
 
     <a href="#" class=" ml-10 mt-20 flex flex-col items-center bg-white  rounded-lg  md:flex-row md:max-w-xl  ">
-        <img class="h-auto max-w-xs rounded" src="https://i.pinimg.com/originals/76/bc/18/76bc18531ec18c6ab04e6b24f8f108ec.jpg" alt="">
+     <img src="{{$src}}"  class="h-auto max-w-xs rounded" >
     <div class="flex flex-col justify-between p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-title tracking-tight text-gray-900 dark:text-">{{$title}}</h5>
     <p class="mb-3 font-common ">{{$description}}</p>
