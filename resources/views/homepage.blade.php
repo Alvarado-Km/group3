d42767a6c2ff230c8a2cb73797cd26c0dad0a297
Alvarado-Km <158789271+Alvarado-Km@users.noreply.github.com>
2024-07-12 15:21:46 -0600

    href

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,71 +1,75 @@
 <x-layout>
 
-    <x-primaryNav> </x-primaryNav>
+   <x-primaryNav> </x-primaryNav>
 
     <!--display-->
     <swiper-container class="h-[80vh] mt-7" pagination="true" pagination-clickable="true" navigation="true"
         slides-per-view="3" space-between="30" centered-slides="true" loop="true" autoplay="true">
-        <swiper-slide class="flex items-center justify-center">
-            <img src="img/display1.jpg" class="h-full w-full object-cover">
-        </swiper-slide>
-        <swiper-slide class="flex items-center justify-center">
-            <img src="img/display2.jpg" class="h-full w-full object-cover">
-        </swiper-slide>
-        <swiper-slide class="flex items-center justify-center">
-            <img src="img/display3.jpg" class="h-full w-full object-cover">
-        </swiper-slide>
-        <swiper-slide class="flex items-center justify-center">
-            <img src="img/display4.jpg" class="h-full w-full object-cover">
-        </swiper-slide>
-        <swiper-slide class="flex items-center justify-center">
-            <img src="img/display5.jpg" class="h-full w-full object-cover">
-        </swiper-slide>
+            <swiper-slide class="flex items-center justify-center">
+                <img src="img/display1.jpg" class="h-full w-full object-cover">
+            </swiper-slide>
+            <swiper-slide class="flex items-center justify-center">
+                <img src="img/display2.jpg" class="h-full w-full object-cover">
+            </swiper-slide>
+            <swiper-slide class="flex items-center justify-center">
+                <img src="img/display3.jpg" class="h-full w-full object-cover">
+            </swiper-slide>
+            <swiper-slide class="flex items-center justify-center">
+                <img src="img/display4.jpg" class="h-full w-full object-cover">
+            </swiper-slide>
+            <swiper-slide class="flex items-center justify-center">
+                <img src="img/display5.jpg" class="h-full w-full object-cover">
+            </swiper-slide>
     </swiper-container>
 
-    <!-- categories -->
+<!-- categories -->
     <div class="columns-4 mt-36 ml-16 mb-16">
         <a href="/products/ropa">
-            <img class="rounded-full col-start-1 h-56" src="img/ropa.jpeg">
+        <img  class="rounded-full col-start-1 h-56" src="img/ropa.jpeg">
         </a>
         <h2 class="font-common grid justify-items-center mr-32  text-xl "> ROPA </h2>
 
         </img>
         <a href="/products/Joyeria">
-            <img class="rounded-full col-start-2 h-56" src="img/Joyeria.jpg">
-            <h2 class="font-common grid justify-items-center mr-32  text-xl"> JOYERIA </h2>
-            </img>
-            <a href="/products/velas">
-                <img class="rounded-full col-start-3 h-56" src="img/Velas.jpg">
-                <h2 class="font-common grid justify-items-center mr-32  text-xl">VELAS</h2>
-                </img>
-                <a href="/products/muebles">
-                    <img class="rounded-full column-end h-56" src="img/muebles.jpg">
-                    <h2 class="font-common grid justify-items-center mr-32 text-xl">MUEBLES</h2>
-                    </img>
+        <img class="rounded-full col-start-2 h-56" src="img/Joyeria.jpg">
+        </a>
+        </img>
+        <h2 class="font-common grid justify-items-center mr-32  text-xl"> JOYERIA </h2>
+
+        <a href="/products/velas">
+        <img class="rounded-full col-start-3 h-56" src="img/Velas.jpg">
+
+    </img></a>
+        <h2 class="font-common grid justify-items-center mr-32  text-xl">VELAS</h2>
+
+
+        <a href="/products/muebles">
+        <img class="rounded-full column-end h-56" src="img/muebles.jpg">
+      </img> </a>
+        <h2 class="font-common grid justify-items-center mr-32 text-xl">MUEBLES</h2>
+
 
     </div class="column-end">
 
     <div class="columns-4 mt-10 ml-16 mb-16 ">
         <a href="/products/ceramica">
-            <img class="rounded-full col-start-1 h-56" src="img/ceramica.jpg">
-        </a>
+        <img class="rounded-full col-start-1 h-56" src="img/ceramica.jpg">
+</img> </a>
         <h2 class="font-common pl-14 mr-32 text-xl">CERAMICA</h2>
-        </img>
-        <a href="/products/instrumentos">
-            <img class="rounded-full col-start-2 h-56" src="img/Instrumentos.jpg">
-            <h2 class="font-common pl-10 mr-32 text-xl">INSTRUMENTOS</h2>
-            </img>
-        </a>
-        <a href="/">
+<a href="/products/instrumentos">
+        <img class="rounded-full col-start-2 h-56" src="img/Instrumentos.jpg">
+ </img> </a>
+        <h2 class="font-common pl-10 mr-32 text-xl">INSTRUMENTOS</h2>
+<a href="/products/vidrios">
         <img class="rounded-full col-start-3 h-56" src="img/Vidrios.jpg">
+        </img> </a>
         <h2 class="font-common pl-16 mr-32 text-xl">VIDRIOS</h2>
-        </img>
-        </a>
-        <a href="">
-            <img class="rounded-full col-start-4 h-56" src="img/Dulces.jpg">
-            <h2 class="font-common pl-20 mr-32 text-xl">DULCES</h2>
-            </img>
-        </a>
+<a href="/products/dulces">
+        <img class="rounded-full col-start-4 h-56" src="img/Dulces.jpg"> </img>
+</a>
+        <h2 class="font-common pl-20 mr-32 text-xl">DULCES</h2>
+
+
     </div>
 
 
--- routes/getKami.php
+++ routes/getKami.php
@@ -43,3 +43,27 @@ Route::get('/products/Joyeria', function(){
 Route::get('/products/ropa', function(){
     return view('products.ropa');
 });
+
+Route::get('/products/dulces', function(){
+    return view('products.dulces');
+});
+
+Route::get('/products/ceramica', function(){
+    return view('products.ceramica');
+});
+
+Route::get('/products/instrumentos', function(){
+    return view('products.instrumentos');
+});
+
+Route::get('/products/muebles', function(){
+    return view('products.muebles');
+});
+
+Route::get('/products/velas', function(){
+    return view('products.velas');
+});
+
+Route::get('/products/vidrios', function(){
+    return view('products.vidrios');
+});
