9799b26adcb67277cf271171a47d4b013be6f42a
Alvarado-Km <158789271+Alvarado-Km@users.noreply.github.com>
2024-07-24 16:14:52 -0600

    home bg changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <div class="h-screen w-screen bg-cover bg-center h-fit" style="background-image: url({{ asset('/img/bgw.png') }}">
+    <div class="h-screen w-screen bg-cover bg-center h-fit" style="background-image: url({{ asset('/img/wbg.png') }}">
 
     <x-primarynav class=""> </x-primarynav>
 
