61b7591e65ac1e8ee7bb9bec12c4403b0d2f1440
lesliediaz16 <leslie.diaz2025@poma.superate.org.sv>
2025-06-13 15:41:09 -0600

    HOMEPAGE
    HOMEPAGE
    Changes in categories.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,6 +2,7 @@
    <x-primarynav/>
 
     <!--display-->
+    <!--
     <swiper-container class="h-[80vh] mt-7" pagination="true" pagination-clickable="true" navigation="true"
         slides-per-view="3" space-between="30" centered-slides="true" loop="true" autoplay="true">
             <swiper-slide class="flex items-center justify-center">
@@ -20,13 +21,189 @@
                 <img src="img/display5.jpg" class="h-full w-full object-cover">
             </swiper-slide>
     </swiper-container>
+    -->
 
+<!-- categories titulo -->
     <div class="static">
-        <h1 class="text-3xl grid place-items-center font-common my-20">Explora nuestras categorías</h1>
+        <h1 class="text-7xl grid place-items-center font-common my-20 text-red-700">Explora nuestras categorías</h1>
+    </div>
+<!-- categories titulo -->
+
+<!-- categories inicio -->
+
+<div class="grid grid-cols-5">
+
+    <!-- elemento1 -->
+    <div class="grid-cols-1">
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/joyeria.png" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class=" flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Joyería y Bisutería</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/vidrio.webp" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Vidrio</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+
     </div>
 
+    <!-- elemento2 -->
+    <div class="grid-cols-2">
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/ceramica.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Cerámica</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl max-h-80" src="/img/categories/instru.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Instrumentos</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- elemento3 -->
+    <div class="grid-cols-3">
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/textiles.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Textiles y bordados</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/casa.png" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Decoración y hogar</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- elemento4 -->
+    <div class="grid-cols-4">
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/velaas.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Velas</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl max-h-80" src="/img/categories/bolsos.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Bolsos carteras</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- elemento5 -->
+    <div class="grid-cols-5">
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class="max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/muuebles.jpg" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Muebles</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class=" gap-y-10 mx-4 mb-16 ml-11">
+
+            <div class=" max-w-sm bg-white border border-gray-200 rounded-xl shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                <a>
+                    <img class="rounded-xl" src="/img/categories/jugueete.webp" alt="" />
+                </a>
+
+                <div class="p-0.5">
+                    <a>
+                    <h5 class="flex items-center justify-center text-lg font-bold tracking-tight text-gray-900 dark:text-white">Juguetes</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- categories final-->
+
     <!-- categories -->
-    <div class="grid grid-cols-4 gap-y-10 mx-12 mb-16">
+        </div>
         @foreach($categories as $category)
             <a href="{{ route('categories', $category->id) }}">
                 <img  class="rounded-full h-56 mx-auto" src="{{ asset($category->img) }}">
