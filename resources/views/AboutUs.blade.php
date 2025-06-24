61ec7b205cadaa0721555d6c1bad0fbf862c0eb2
Elisa-lopez <153461620+Elisa-lopez@users.noreply.github.com>
2025-06-24 16:12:01 -0600

    product

--- resources/views/AboutUs.blade.php
+++ resources/views/AboutUs.blade.php
@@ -1,35 +1,21 @@
 <x-layout>
     <x-primaryNav/>
 
-    <div class="bg-slate-50">
-        <section
-            class="bg-center bg-scroll pt-10 h-screen bg-[url('https://i.pinimg.com/originals/4d/fc/2c/4dfc2c8697a77b385ad47498c6499182.gif')] bg-slate-50 bg-blend-multiply">
 
-            <div class="grid grid-cols-6 grid-rows-1">
-                <div class="grid col-start-1 col-span-3 place-items-center ">
-                    <img src="img/imagen.jpg">
-                </div>
-
-                <div class="grid place-items-center col-start-4 col-span-6">
-                    <h2 class="text-4xl font-title mt-5"> ¿Quiénes somos? </h2>
-                    <p class="text-xl mx-36 text-justify font-common -mt-20">Somos Artesía, y nos dedicamos a
+        <section>
+                    <h2 class=" text-red-600 text-4xl text-center"> Acerca de nosotros </h2>
+                    <p Somos Artesía, y nos dedicamos a
                         preservar y promover el arte artesanal, colaborando directamente con
                         talentosos artesanos para ofrecer productos únicos y de alta calidad. Valoramos la
                         sostenibilidad y el comercio justo, creando un impacto positivo en las comunidades artesanas. Descubre piezas auténticas y
-                        hechas a mano que cuentan historias y reflejan dedicación.</p>
-                </div>
+                        hechas a mano que cuentan historias y reflejan dedicación.>
+               
             </div>
         </section>
-        <img src="img/definitive.jpg" class="w-screen">
-
-        <section
-            class="bg-center bg-scroll h-screen bg-[url('https://i.pinimg.com/originals/4d/fc/2c/4dfc2c8697a77b385ad47498c6499182.gif')] bg-slate-50 bg-blend-multiply">
+        <section>
             <h1 class=" font-title text-4xl grid place-items-center pt-20 mb-20">Sobre nosotros </h1>
 
-            <div class="columns-2 absolute ">
-                <div>
-                    <img src="/img/11.gif" class="col-start-1 pt-6 pl-20 w-fit h-fit grid justify-items-center relative"
-                        autoplay loop> </img>
+            <div class="columns-2 absolute "><div>
                     <p class="font-title grid place-items-center column-start-2 text-xl pt-16"> Mission</p>
                     <p class="text-xl mb-16 mx-32 text-justify font-common mt-8"> Conservar el arte y la cultura a
                         través de una plataforma digital de los artesanos salvadoreños en las nuevas generaciones.
@@ -41,39 +27,31 @@
             </div>
         </section>
 
-        <section
-            class="bg-center pt-20  bg-[url('https://i.pinimg.com/originals/4d/fc/2c/4dfc2c8697a77b385ad47498c6499182.gif')] bg-slate-50 bg-blend-multiply">
-
-            <h1 class="font-title text-4xl grid justify-items-center pt-12">Nuestros valores</h1>
+        <section>
+            <h1 class="font-title text-4xl grid justify-items-center pt-12">Nuestros valores </h1>
 
             <div class="grid grid-cols-3 font-common text-3xl mt-24 mb-10">
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number1.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify font-common mr-6"> Autenticidad </p>
                 </div>
 
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number2.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify font-common mr-6">Creatividad </p>
                 </div>
 
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number3.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify font-common mr-6">Compromiso </p>
                 </div>
 
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number4.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify  mr-6">Respeto cultural </p>
                 </div>
 
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number5.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify  mr-6">
                         Colaboración </p>
                 </div>
-                <div class="flex items-center mx-auto space-x-4 my-6">
-                    <img class="rounded-full h-32" src="img/number6.png">
+                <div class="flex items-center mx-auto space-x-4 my-6"></div>
                     <p class=" text-justify mr-6">
                         Innovación </p>
                 </div>
