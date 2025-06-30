2d4d0f630d2f54bdbcba63a5018d79cae1470e06
alison-diaz123 <adiazazucena@gmail.com>
2025-06-30 14:52:03 -0600

    final-profileArtisan

--- resources/views/users/ProfileArtisan.blade.php
+++ resources/views/users/ProfileArtisan.blade.php
@@ -1,24 +1,162 @@
 <x-layout>
-   <x-allnav/>
+    <x-allnav />
 
-    <div class="grid grid-cols-6 mx-12 mb-16">
+    <div class="flex justify-center ">
+     <h2 class="text-2xl font-bold text-red-800 mb-4">Productos de Lindsay Valle</h2>
+    </div>
+
+    <div class="grid grid-cols-6 mb-16 mx-12">
         <div class="mt-5 col-span-2 mr-10">
-          <img class="mx-auto cursor-pointer w-64 h-64 rounded-full" src="/img/profilepic.jpg" alt="Rounded avatar">
-          <h1 class="font-common text-2xl border boder-gray bg-grass rounded text-center mt-3 py-1">{{ $user->name }}</h1>
-          <p class="font-common mt-3 text-justify text-1xl">{{ $user->description }}</p>
+            <!-- Profile-->
+            <div class=" flex items-center">
+                <img class="w-20 h-20 rounded-full  " src="/img/xio.jpg" alt="">
+                <div class="font-bold ml-5 text-black ">
+                    <div>Lindsay valle</div>
+                    <div class="text-sm  text-gray-500 text-gray-400">Artesana (10 años)</div>
+                </div>
+            </div>
+
+            <!--descrition-->
+            <div class=" bg-white border border-red-900 rounded-lg shadow p-4 col-span-1 mt-5">
+                <h2 class="text-red-700 font-bold mt-5 ml-5">MI HISTORIA</h2>
+                <p class="font-common mt-3 text-justify text-1xl ml-5 mr-5">
+                    Soy Lindsay valle, nací en San Salvador y crecí con mis cinco hermanos. Desde pequeño me gustaron
+                    las manualidades, y en 1964 tomé clases de cerámica.
+                    Estudié arquitectura en la Universidad de El Salvador, pero también sentí el deseo de ser sacerdote,
+                    por lo que viajé a Colombia, Francia y Bélgica para estudiar teología.
+                    Fue en Francia donde decidí dedicarme al arte. Más tarde regresé a El Salvador para enfocarme en la
+                    pintura y fundar un centro de arte. En 1971, además de exponer mi obra,
+                    formé parte de la Banda del Sol.
+                    Por una canción con mensaje antimilitarista, estuve encarcelado
+                    tres días.
+                </p>
+
+            </div>
+             <button  class=" bg-white rounded-xl border  p-10 shadow flex flex-col items-center mt-5 border border-red-900">
+             <h2 class="text-red-700 font-bold mt-5 ml-5 flex justify-center mt-5 ">Ubicación</h2>
+             <img class=" rounded-full mt-5 mb-5 h-auto max-w-xs size-60 w-40 h-40  ml-" src="/img/map.jpg">
+              <button>
+
         </div>
 
+     <!--Imagenes-->
+    <div class="col-span-4 mt-40 grid grid-cols-3">
+    <!--targeta 1-->
+    <div class="bg-white rounded-lg shadow p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/8.jpg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">Cartera de manta</h3>
+    <p class="text-sm text-gray-500">$10</p>
+    </div>
+
+        <!--targeta 2-->
+    <div class="bg-white rounded-lg shadow p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/tecomata.jpg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">Tecomata</h3>
+    <p class="text-sm text-gray-500">$5.00</p>
+    </div>
+
+        <!--targeta 3-->
+    <div class="bg-white rounded-lg shadow p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/joyero de flor.jpg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">joyero con flores</h3>
+    <p class="text-sm text-gray-500">$10.00</p>
+    </div>
+
+        <!--targeta 4-->
+    <div class="bg-white  mt-5 rounded-lg shadow p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/joyero de corazon.webp" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">joyero con forma de corazón</h3>
+    <p class="text-sm text-gray-500">$12.00</p>
+    </div>
+
+        <!--targeta 5-->
+    <div class="bg-white rounded-lg mt-5 shadow p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/jarroncudrado.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">Jarron cuadrado</h3>
+    <p class="text-sm text-gray-500">$30.00</p>
+    </div>
+
+        <!--targeta 6-->
+    <div class="bg-white rounded-lg shadow mt-5 p-4 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/jarron redondo.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">jarron redondo</h3>
+    <p class="text-sm text-gray-500">$30.00</p>
+    </div>
+
+        <!--targeta 7-->
+    <div class="bg-white rounded-lg shadow p-4 mt-5 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/jarron cristal.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">jarron de cristal</h3>
+    <p class="text-sm text-gray-500">$50.00</p>
+    </div>
+
+        <!--targeta 8-->
+    <div class="bg-white rounded-lg shadow p-4 mt-5 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/jarron azul.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">Jarron con detalles azules</h3>
+    <p class="text-sm text-gray-500">$20.00</p>
+    </div>
+
+        <!--targeta 9-->
+    <div class="bg-white rounded-lg shadow p-4 mt-5 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/canasta de xio.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg"> canasta de minbre de colores</h3>
+    <p class="text-sm text-gray-500">$16.00</p>
+    </div>
+
+        <!--targeta 10-->
+    <div class="bg-white rounded-lg shadow p-4 mt-5 relative">
+    <div class="bg-gray-200 h-32 flex items-center justify-center rounded mb-4">
+      <img src="/img/canasta de xio 2.jpeg" class="h-full w-full object-cover">
+    </div>
+    <h3 class="font-semibold text-lg">canasta de minbre</h3>
+    <p class="text-sm text-gray-500">$15.00</p>
+    </div>
+
+
+    </div>
+
+ <!--Map-->
+
+
+
+
+
+    <!--
+          <h1 class="">{{ $user->name }}</h1>
+          <p class="font-common mt-3 text-justify text-1xl">{{ $user->description }}</p>
+         </div>
+
             <div class="mt-5 mr-40 grid grid-cols-4 gap-64">
-                @foreach($user->products->chunk(2) as $blocks)
-                    <div>
-                        @foreach($blocks as $product)
-                            <a href="{{ route('handicrafts', $product->id) }}" class="mt-5 flex flex-col items-center bg-white  rounded-lg  md:flex-row md:max-w-xl  ">
+                @foreach ($user->products->chunk(2) as $blocks)
+<div>
+                        @foreach ($blocks as $product)
+<a href="{{ route('handicrafts', $product->id) }}" class="mt-5 flex flex-col items-center bg-white  rounded-lg  md:flex-row md:max-w-xl  ">
                                 <img class="h-auto max-w-xs size-60 rounded" src='{{ asset("img/productos/$product->category_id.$product->id.jpeg") }}' alt="">
                             </a>
-                        @endforeach
+@endforeach
                     </div>
-                @endforeach
+@endforeach
             </div>
         </div>
     </div>
+    -->
 </x-layout>
